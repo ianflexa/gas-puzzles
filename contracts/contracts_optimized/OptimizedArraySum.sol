5286c5cd75b98f33c5b9f6de1239fe1da630c149
ianflexa <ianflexa@gmail.com>
2023-11-25 09:47:35 -0400

    op array sum

---
+++ contracts/contracts_optimized/OptimizedArraySum.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+contract OptimizedArraySum {
+    // Do not modify this
+    uint256[] array;
+
+    // Do not modify this
+    function setArray(uint256[] memory _array) external {
+        require(_array.length <= 10, 'too long');
+        array = _array;
+    }
+
+    // optimize this function
+    function getArraySum() external view returns (uint256 sum) {
+        uint256 length = array.length;
+        for (uint256 i; i < length;) {
+            sum += array[i];
+            unchecked { ++i; }
+        }
+    }
+}
