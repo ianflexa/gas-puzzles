5c3cb2fe15df55b5b48b2b645b23aeaf6bdd351a
ianflexa <ianflexa@gmail.com>
2023-11-25 10:30:55 -0400

    op require

--- contracts/contracts_optimized/OptimizedRequire.sol
+++ contracts/contracts_optimized/OptimizedRequire.sol
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.15;
 
-contract Require {
+error InvalidAmount();
+
+contract OptimizedRequire {
     // Do not modify these variables
     uint256 constant COOLDOWN = 1 minutes;
     uint256 lastPurchaseTime;
 
     // Optimize this function
     function purchaseToken() external payable {
-        require(
-            msg.value == 0.1 ether &&
-                block.timestamp > lastPurchaseTime + COOLDOWN,
-            'cannot purchase'
-        );
+        if (msg.value != 0.1 ether) revert InvalidAmount();
+        unchecked {
+            require(
+                block.timestamp - COOLDOWN > lastPurchaseTime,
+                'cannot purchase'
+            );
+        }
         lastPurchaseTime = block.timestamp;
-        // mint the user a token
     }
 }
